e750b3a800270b5c6397bfcd4a489b7aabe63c93
mathieu Lawson <mathieuawukud23@gmail.com>
2021-09-25 17:38:04 +0200

    25/09/2021

--- src/Controller/Comm/accueilController.php
+++ src/Controller/Comm/accueilController.php
@@ -9,7 +9,9 @@ use App\Entity\Appel;
 use App\Entity\Personne;
 use App\Entity\Commissaire;
 use App\Form\CommissaireType;
+use App\Form\registerNomType;
 use App\Repository\AppelRepository;
+use App\Repository\DatahubRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Picqer\Barcode\BarcodeGeneratorHTML;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,6 +21,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
@@ -337,17 +340,14 @@ public function SearchDays(Request $request,AppelRepository $appeldata): Respons
 
               $call = $appeldata->AppelByDate3($personne, $debut, $fin);
           
-             foreach($call as $call1){
+             
 
-                     $nom= $call1->getPeronneOne()->getNom();
-                     $num = $call1->getPeronneOne()->getContact();
-             }
+                    
 
 
               return $this->render('comm/searchOneCible.html.twig',[
                 'call'=> $call,
-                'nom'=>$nom,
-                'num'=>$num
+                
                
               ]);
           
@@ -357,11 +357,6 @@ public function SearchDays(Request $request,AppelRepository $appeldata): Respons
              
 
           }
-        // dd($call);
-      
-      
-        
-      
       
       
       
@@ -369,10 +364,186 @@ public function SearchDays(Request $request,AppelRepository $appeldata): Respons
       
       
       
+      
+        
+      
+    
+        
+
+
+
+         /**
+         * @Route("/importCible/", name="comm_import_cible")
+         * @param Request $request
+         * @throws \Exception
+         */
+
+       
+        public function ImportCible(Request $request,  EntityManagerInterface $entityManager,DatahubRepository $hubdata):Response
+        {
+           
+        
+        
+        $user = $this->getUser();
+        
+             
+           
+           
+            if (!empty($_POST)) {
+        
+                //$path= 'C:\xampp\htdocs\exporTest\src\Controller/../../public/uploads/c3900ad16dba7bc3f827f364561dce25releve.csv';
+                $entityManager = $this->getDoctrine()->getManager();
+                $file = $request->files->get('fichier');
+                //dd($file);
+               // $fileName = md5(uniqid()).'.'.$file->guessExtension();
+             //   $fileFolder= $file->move($this->getParameter('upload_directory'),$fileName);
+               // $fileFolder = __DIR__ . '/../../public/uploads/';  //choose the folder in which the uploaded file will be stored
+                $fileFolder = 'C:/xampp/htdocs/exporTest/public/uploads/';
+                $filePathName = md5(uniqid()) . $file->getClientOriginalName();
+               
+               
+              
+              
+        
+                try {
+                    $file->move($fileFolder, $filePathName);
+                } catch (FileException $e) {
+                    dd($e);
+                }
+                
+                
+               // dd($fileFolder."".$filePathName);
+        
+        
+        
+        
+        
+              // $hubdata->insertCsvData($entityManager,$fileFolder."".$filePathName);
+                $hubdata->datahubAb($entityManager);
+                  $hubdata->datahubAp($entityManager);
+                  
+                 $hubdata->datahubUpdateAb($entityManager);
+                 $hubdata->datahubUpdateAp($entityManager);
+                  $hubdata->datahubUpdateIdentiteAppele($entityManager);
+                $hubdata->datahubUpdateDate($entityManager);
+                $hubdata->datahubUpdateHeure($entityManager);
+                 $hubdata->datahubUpdateDuree($entityManager);
+                $hubdata->datahubUpdateTypeAppel($entityManager);
+                 $hubdata->datahubUpdateSensAppel($entityManager);
+                  $hubdata->datahubUpdateIMEI($entityManager);
+                  $hubdata->datahubUpdateIMSI($entityManager);
+                 $hubdata->datahubUpdateLocalisation($entityManager);
+                 $hubdata->insertNumeroAb($entityManager);
+                  $hubdata->insertNumeroApp($entityManager);    
+                $hubdata->insertNumOp($entityManager);
+                  $hubdata->insertpersonneAb($entityManager);
+                  $hubdata->insertpersonneAp($entityManager);
+                    
+                 $hubdata->insertAntenne($entityManager);
+                  $hubdata->insertPortable($entityManager);
+                  $hubdata->insertIdNumToPerson($entityManager); 
+                  $hubdata->insertAppel($entityManager);
+                 
+                
+                
+              
+                
+                  return $this->redirectToRoute("comm_import_success");
+                   
+            }
+          
+        
+            return $this->render('comm/import.html.twig', ['user'=>$user]);
+                
+        }
+        
+  
+      
+
+
+
+        /**
+     * @Route("/import_succes",name="comm_import_success")
+     */
+
+    public function importSuccess(Request $request)
+    {
+     
+
+        
+        
+
+        
+        return $this->render('comm/traitementOK1.html.twig',[
+            
+        ] );
+    }
 
 
 
 
+
+
+
+
+
+    /**
+     * @Route("/addName",name="comm_add_name")
+     */
+
+    public function addName(Request $request):Response
+    {
+
+        $personne = new Personne();
+
+        $form = $this->createForm(registerNomType::class, $personne);
+        $form->handleRequest($request);
+        $user = $this->getUser();
+       // $form->handleRequest($request);
+        $em = $this->getDoctrine()->getManager();
+         
+        if ($form->isSubmitted() && $form->isValid()) {
+            $num= $form->get('numero')->getData();
+            $nom= $form->get('nom')->getData();
+            
+            $cas= $this->getDoctrine()->getRepository(Personne::class)->findOneBy(['contact'=> $num]);
+            $cas->setNom($nom);
+            $em->persist($cas);
+            $em->flush();
+
+            
+
+            return $this->redirectToRoute("comm_add_success");
+
+        }
+
+        return $this->render('comm/addName.html.twig',[
+            'user'=>$user,
+            'form' => $form->createView()
+        ]);
+            
+    }
+
+
+    
+
+
+     /**
+     * @Route("/add_succes",name="comm_add_success")
+     */
+
+    public function AddSuccess(Request $request)
+    {
+     
+
+        
+        
+
+        
+        return $this->render('comm/traitementOK2.html.twig',[
+            
+        ] );
+    }
 
      
      
